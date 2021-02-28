1271bf2b5ffa387364a5fc628635583c832f1cec
Amanda <ajnikrant@gmail.com>
2021-02-28 14:02:12 -0500

    commit tree 948aa9b480108b9ffe9220ce5cee2eb62dbcbe43

--- config/environment.rb
+++ config/environment.rb
@@ -2,3 +2,19 @@ require 'pry'
 require_relative '../app/models/artist.rb'
 require_relative '../app/models/gallery.rb'
 require_relative '../app/models/painting.rb'
+
+artist1=Artist.new("Ralph", 10)
+artist2=Artist.new("jane", 50)
+artist3=Artist.new("alex", 2)
+
+gallery1=Gallery.new("fancy place", "chicago")
+gallery2=Gallery.new("nice place", "miami")
+
+painting1=Painting.new("moods", 3000, artist1, gallery1)
+painting2=Painting.new("colors", 10, artist2, gallery2)
+painting3=Painting.new("wind", 33333333333, artist1, gallery2)
+
+
+
+
+binding.pry
